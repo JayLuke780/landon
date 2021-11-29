fe1b5873d72f6ae10435bcb1f8bf3ca7e720e77a
Jay Lucas <jalucas@deloitte.com>
2021-11-29 14:05:28 -0500

    update to boilerplate

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and aws code.
+          Learn React and aws code. 
         </a>
       </header>
     </div>
